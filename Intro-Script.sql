df3a7f57e52b271cfac9e68d9f28be2feac0c3e3
Omar <omaraakamal@gmail.com>
2025-04-27 19:37:47 +0300

    SQL update

--- Intro-Script.sql
+++ Intro-Script.sql
@@ -1,5 +1,9 @@
+-- creating a new Table in our DB
 CREATE TABLE bands(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR NOT NULL,
 	genre VARCHAR
 );
+
+-- Add data to the table
+INSERT INTO bands(name,genre) VALUES'('The Beatles','Rock');
